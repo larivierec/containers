a3f1a98b2b5736420ef903716ee121051bcc639d
Christopher Larivière <lariviere.c@gmail.com>
2023-09-21 22:09:10 -0400

    add protonmail bridge to containers
    Signed-off-by: Christopher Larivière <lariviere.c@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # containers
+
+Various containers used throught my cluster and other projects.
---
+++ apps/protonmail-bridge-docker/entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -ex
+
+# Initialize
+if [[ $1 == init ]]; then
+
+    # Initialize pass
+    gpg --generate-key --batch /protonmail/gpgparams
+    pass init pass-key
+    
+    # Kill the other instance as only one can be running at a time.
+    # This allows users to run entrypoint init inside a running conainter
+    # which is useful in a k8s environment.
+    # || true to make sure this would not fail in case there is no running instance.
+    pkill proton-bridge || true
+
+    # Login
+    /protonmail/proton-bridge --cli $@
+
+else
+
+    # socat will make the conn appear to come from 127.0.0.1
+    # ProtonMail Bridge currently expects that.
+    # It also allows us to bind to the real ports :)
+    socat TCP-LISTEN:25,fork TCP:127.0.0.1:1025 &
+    socat TCP-LISTEN:143,fork TCP:127.0.0.1:1143 &
+
+    # Start protonmail
+    # Fake a terminal, so it does not quit because of EOF...
+    rm -f faketty
+    mkfifo faketty
+    cat faketty | /protonmail/proton-bridge --cli $@
+
+fi
