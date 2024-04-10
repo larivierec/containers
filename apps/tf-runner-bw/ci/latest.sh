b4119d8c85e7251791681cd2fe1deb42660b8ffa
Christopher Larivière <lariviere.c@gmail.com>
2024-04-10 17:28:30 -0400

    update tofu-controller
    Signed-off-by: Christopher Larivière <lariviere.c@gmail.com>

--- apps/tf-runner-bw/ci/latest.sh
+++ apps/tf-runner-bw/ci/latest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-version="$(curl -sX GET "https://api.github.com/repos/weaveworks/tf-controller/tags" | jq --raw-output '.[0].name' 2>/dev/null)"
+version="$(curl -sX GET "https://api.github.com/repos/flux-iac/tofu-controller/tags" | jq --raw-output '.[0].name' 2>/dev/null)"
 version="${version#*v}"
 printf "%s" "${version}"
