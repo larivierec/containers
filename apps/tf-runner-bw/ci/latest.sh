7c5f7d29b3c697d80ea0f707cfc63db05f25bcb9
Christopher Larivière <lariviere.c@gmail.com>
2024-10-11 13:00:43 -0400

    deprecate tf-runner-bw
    Signed-off-by: Christopher Larivière <lariviere.c@gmail.com>

--- apps/tf-runner-bw/ci/latest.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-version="$(curl -sX GET "https://api.github.com/repos/flux-iac/tofu-controller/tags" | jq --raw-output '.[0].name' 2>/dev/null)"
-version="${version#*v}"
-printf "%s" "${version}"
